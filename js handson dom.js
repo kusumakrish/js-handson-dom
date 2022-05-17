ae2b046510faba511c2128a3d011473c067cf857
kusumakrish <80766178+kusumakrish@users.noreply.github.com>
2022-05-17 20:13:49 +0530

    Update js handson dom.js

--- js handson dom.js
+++ js handson dom.js
@@ -33,6 +33,52 @@ function replaceText(){
 //document -The document object is a property of the window object.The document object represents the HTML document loaded in that window.
 
 
+function form1(){
+    var form = document.createElement("form")
+    form.setAttribute("method","get")
+    form.setAttribute("action","submit")
+    var name = document.createElement("input");
+    name.setAttribute("type","text");
+    name.setAttribute("name","Fullname");
+    name.setAttribute("placeholder","Fullname");
+    // name.setAttribute("required");
+
+    var email = document.createElement("input");
+    email.setAttribute("type","email");
+    email.setAttribute("name","email");
+    email.setAttribute("placeholder","email@prepbyte.com");
+    email.setAttribute("pattern","[a-zA-Z0-9]+@prepbyte.com");
+    
+    var phoneNo = document.createElement("input");
+    phoneNo.setAttribute("type","tel");
+    phoneNo.setAttribute("name","phoneNo");
+    phoneNo.setAttribute("placeholder","91-87952636");
+    phoneNo.setAttribute("pattern","[91]{2}[0-9]{10}");
+    // phoneNo.setAttribute("required");
+
+    var DOB = document.createElement("input");
+    DOB.setAttribute("type","date");
+    DOB.setAttribute("name","DOB");
+    DOB.setAttribute("placeholder","DOB");
+    DOB.setAttribute("min","1995-01-01");
+    // DOB.setAttribute("required");
+
+    var submit = document.createElement("input")
+    submit.setAttribute("type","submit")
+    submit.setAttribute("value","submit")
+    form.appendChild(name);
+    form.appendChild(email);
+    form.appendChild(phoneNo);
+    form.appendChild(DOB);
+    form.appendChild(submit);
+
+    document.getElementsByClassName("form")[0]
+    .appendChild(form);
+   
+}
+form1();
+
+
 function getoption(){
     // let result4 = document.getElementById("dropdown");
     // console.log(result4.innerText);
